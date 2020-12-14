9236c1514bf70cd780f61f2c5553e74fc41b8fb7
Amartya Aishwarya <amartya970914@gmail.com>
2020-12-14 18:13:06 +0530

    wip

--- src/app/add-item-form/add-item-form.component.ts
+++ src/app/add-item-form/add-item-form.component.ts
@@ -8,13 +8,23 @@ import { BudgetItem } from 'src/shared/models/budget-item.model';
   styleUrls: ['./add-item-form.component.scss'],
 })
 export class AddItemFormComponent implements OnInit {
-  @Input() item: BudgetItem = new BudgetItem('', null);
+  @Input() item: BudgetItem;
   @Output() formSubmit: EventEmitter<BudgetItem> = new EventEmitter<
     BudgetItem
   >();
+  isNewItem: boolean;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    if (this.item) {
+      //this means that an existing item object was passed into this component
+      //therefore this is not a new item object
+      this.isNewItem = false;
+    } else {
+      this.isNewItem = true;
+      this.item = new BudgetItem('', null);
+    }
+  }
 
   onSubmit(form: NgForm) {
     // console.log(form);
